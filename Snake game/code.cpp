8bb29d054dd6df5660a6fd9196b3a422d2738bfe
minkhoaa <23520757@gm.uit.edu.vn>
2024-05-13 20:26:34 +0700

    Thay đổi tên các hàm giống với báo cáo, thay đổi thứ tự các hàm, thêm comment

--- Snake game/code.cpp
+++ Snake game/code.cpp
@@ -1,17 +1,16 @@
 #include"mySnake.h"
 using namespace std;
 
-//--------- snake 1 người chơi --------------------
-int Snake::kyluc = 0; 
-void Snake::Play() 
+int Snake::kyluc = 0;
+void Snake::Play()
 {
-	system("cls"); 
+	system("cls");
 
 	Setting();
 
 	ShowCur(0);
 
-	//build gamex
+	//build game
 	VeTuong();
 	KhoiTaoRan();
 
@@ -30,7 +29,7 @@ void Snake::Play()
 	int check = 3;
 
 	//Play
-	while (true) 
+	while (true)
 	{
 		//xóa đuôi
 		gotoXY(toa_dox[sl], toa_doy[sl]);
@@ -41,31 +40,31 @@ void Snake::Play()
 
 		//điều khiển
 			//sử dụng các phím mũi tên hoặc w, a, s, d để di chuyển rắn
-		if (_kbhit()) 
+		if (_kbhit())
 		{
 			char c = _getch();
-			if (c == -32) 
+			if (c == -32)
 			{
 				c = _getch();
-				if      (c == 72 && check != 0) check = 1;
+				if (c == 72 && check != 0) check = 1;
 				else if (c == 80 && check != 1) check = 0;
 				else if (c == 75 && check != 3) check = 2;
 				else if (c == 77 && check != 2) check = 3;
 			}
-			if      ((c == (int)'w' || c == (int)'W') && check != 0) check = 1;
-			else if ((c == (int)'s'|| c == (int)'S') && check != 1) check = 0;
+			if ((c == (int)'w' || c == (int)'W') && check != 0) check = 1;
+			else if ((c == (int)'s' || c == (int)'S') && check != 1) check = 0;
 			else if ((c == (int)'a' || c == (int)'A') && check != 3) check = 2;
-			else if ((c == (int)'d' || c == (int)'D') && check !=2) check = 3;
+			else if ((c == (int)'d' || c == (int)'D') && check != 2) check = 3;
 		}
-		if      (check == 3) x++;
+		if (check == 3) x++;
 		else if (check == 2) x--;
 		else if (check == 1) y--;
 		else if (check == 0) y++;
 
 		//kiểm tra biên
-		if (GameOver(toa_dox[0], toa_doy[0])) 
+		if (GameOver(toa_dox[0], toa_doy[0]))
 		{
-			SetColor(12); 
+			SetColor(12);
 
 			system("cls");
 			std::cout << "Game over";
@@ -77,11 +76,11 @@ void Snake::Play()
 		DiChuyen(x, y);
 		Sleep(sleep);
 	}
-		system("cls"); 
-		if (GameOver(toa_dox[0], toa_doy[0]) == true) 
-		{
-		if (score >= kyluc) 
-		kyluc = score; 
+	system("cls");
+	if (GameOver(toa_dox[0], toa_doy[0]) == true)
+	{
+		if (score >= kyluc)
+			kyluc = score;
 
 		SetColor(11);
 		for (int i = 10; i <= 40; i++)
@@ -99,43 +98,43 @@ void Snake::Play()
 			std::cout << (char)176;
 		}
 		gotoXY(10, 0);
-		std::cout << "GAME OVER"; 
+		std::cout << "GAME OVER";
 		gotoXY(12, 5); std::cout << "PRESS M TO RETURN MAIN MENU";
 		gotoXY(12, 6); std::cout << "PRESS R TO WATCH RECORD";
 		int temp = _getch();
-		if (temp == (int)'m') 
+		if (temp == (int)'m')
 		{
-			sl = 1; 
+			sl = 1;
 			score = 0;
 			Play();
-		} 
+		}
 		if (temp == (int)'r' || temp == (int)'R')
 		{
 			for (int i = 11; i <= 39; i++) {
 				gotoXY(i, 5); std::cout << " ";
 			}
-			gotoXY(12, 5); std::cout << "YOUR RECORD: " << kyluc; 
-			gotoXY(12, 6); std::cout << "PRESS M TO RETURN MAIN MENU"; 
+			gotoXY(12, 5); std::cout << "YOUR RECORD: " << kyluc;
+			gotoXY(12, 6); std::cout << "PRESS M TO RETURN MAIN MENU";
 			score = 0;
-			sl = 1; 
+			sl = 1;
 			int cur = _getch();
-			if (cur == (int)'m'|| cur == (int)'m') Play();
+			if (cur == (int)'m' || cur == (int)'m') Play();
 		}
-	} 
+	}
 }
 
 //vẽ tường
-void Snake::VeTuong() 
+void Snake::VeTuong()
 {
 	SetColor(11);
-	for (int i = 10; i <= 100; i++) 
+	for (int i = 10; i <= 100; i++)
 	{
 		gotoXY(i, 1);
 		std::cout << (char)176;
 		gotoXY(i, 25);
 		std::cout << (char)176;
 	}
-	for (int i = 1; i <= 25; i++) 
+	for (int i = 1; i <= 25; i++)
 	{
 		gotoXY(10, i);
 		std::cout << (char)176;
@@ -145,11 +144,11 @@ void Snake::VeTuong()
 }
 
 //khởi tạo rắn
-void Snake::KhoiTaoRan() 
+void Snake::KhoiTaoRan()
 {
 	int khoi_tao_x = 50;
 	int khoi_tao_y = 13;
-	for (int i = 0; i < sl; i++) 
+	for (int i = 0; i < sl; i++)
 	{
 		toa_dox[i] = khoi_tao_x--;
 		toa_doy[i] = khoi_tao_y;
@@ -157,10 +156,10 @@ void Snake::KhoiTaoRan()
 }
 
 //vẽ rắn
-void Snake::VeRan() 
+void Snake::VeRan()
 {
 	SetColor(mau_ran);
-	for (int i = 0; i < sl; i++) 
+	for (int i = 0; i < sl; i++)
 	{
 		gotoXY(toa_dox[i], toa_doy[i]);
 		if (i == 0) std::cout << (char)254;
@@ -169,9 +168,9 @@ void Snake::VeRan()
 }
 
 //di chuyển
-void Snake::DiChuyen(int x, int y) 
+void Snake::DiChuyen(int x, int y)
 {
-	for (int i = sl; i > 0; i--) 
+	for (int i = sl; i > 0; i--)
 	{
 		toa_dox[i] = toa_dox[i - 1];
 		toa_doy[i] = toa_doy[i - 1];
@@ -181,7 +180,7 @@ void Snake::DiChuyen(int x, int y)
 }
 
 //kiểm tra game over
-bool Snake::GameOver(int x, int y) 
+bool Snake::GameOver(int x, int y)
 {
 	if (toa_dox[0] == 10 || toa_dox[0] == 100 || toa_doy[0] == 1 || toa_doy[0] == 25)
 		return true;
@@ -189,18 +188,18 @@ bool Snake::GameOver(int x, int y)
 }
 
 //tạo quả 
-void Snake::TaoQua() 
+void Snake::TaoQua()
 {
 	this->xqua = rand() % (99 - 11 + 1) + 11;
 	this->yqua = rand() % (24 - 2 + 1) + 2;
-	if (KtRanDeQua()) 
+	if (KtRanDeQua())
 	{
 		TaoQua();
 	}
 }
 
 //vẽ quả
-void Snake::VeQua() 
+void Snake::VeQua()
 {
 	SetColor(15);
 	gotoXY(xqua, yqua); std::cout << '*';
@@ -208,26 +207,26 @@ void Snake::VeQua()
 }
 
 //kiểm tra rắn ăn quả
-bool Snake::KtRanAnQua() 
+bool Snake::KtRanAnQua()
 {
 	return  (toa_dox[0] == xqua && toa_doy[0] == yqua);
 }
 
 // xử lí khi rắn ăn quả
-void Snake::XuLiRanAnQua() 
+void Snake::XuLiRanAnQua()
 {
-	if (KtRanAnQua()) 
+	if (KtRanAnQua())
 	{
 		sl++;
 		score += tocdo;
 		qua_to++;
-		if (qua_to % 5 != 0) 
+		if (qua_to % 5 != 0)
 		{
 			TaoQua();
 			VeQua();
 			XuatDiem();
 		}
-		else if (qua_to % 5 == 0) 
+		else if (qua_to % 5 == 0)
 		{
 			TaoQuaTo();
 			VeQuaTo();
@@ -236,9 +235,9 @@ void Snake::XuLiRanAnQua()
 }
 
 //kiểm tra quả xuất hiện có trùng với rắn không
-bool Snake::KtRanDeQua() 
+bool Snake::KtRanDeQua()
 {
-	for (int i = 0; i < sl; i++) 
+	for (int i = 0; i < sl; i++)
 	{
 		if (toa_dox[i] == xqua && toa_doy[i] == yqua) return true;
 	}
@@ -246,9 +245,9 @@ bool Snake::KtRanDeQua()
 }
 
 //kiểm tra rắn chạm thân
-bool Snake::KtRanChamThan() 
+bool Snake::KtRanChamThan()
 {
-	for (int i = 4; i < sl; i++) 
+	for (int i = 4; i < sl; i++)
 	{
 		if (toa_dox[0] == toa_dox[i] && toa_doy[0] == toa_doy[i]) return true;
 	}
@@ -257,7 +256,7 @@ bool Snake::KtRanChamThan()
 
 
 
-void Snake::TaoQuaTo() 
+void Snake::TaoQuaTo()
 {
 	this->xqua_to = rand() % (98 - 12 + 1) + 12;
 	this->yqua_to = rand() % (23 - 3 + 1) + 3;
@@ -265,11 +264,11 @@ void Snake::TaoQuaTo()
 		TaoQuaTo();
 }
 
-void Snake::VeQuaTo() 
+void Snake::VeQuaTo()
 {
-	if (!KtRanDeQuaTo()) 
+	if (!KtRanDeQuaTo())
 	{
-		for (int i = xqua_to - 1; i <= xqua_to + 1; i++) 
+		for (int i = xqua_to - 1; i <= xqua_to + 1; i++)
 		{
 			for (int j = yqua_to - 1; j <= yqua_to + 1; j++)
 			{
@@ -280,14 +279,14 @@ void Snake::VeQuaTo()
 	}
 }
 
-void Snake::XuLiRanAnQuaTo() 
+void Snake::XuLiRanAnQuaTo()
 {
-	if (KtRanAnQuaTo()) 
+	if (KtRanAnQuaTo())
 	{
 		sl++;
 		score += 10;
 		qua_to++;
-		for (int i = xqua_to - 1; i <= xqua_to + 1; i++) 
+		for (int i = xqua_to - 1; i <= xqua_to + 1; i++)
 		{
 			for (int j = yqua_to - 1; j <= yqua_to + 1; j++)
 			{
@@ -301,9 +300,9 @@ void Snake::XuLiRanAnQuaTo()
 	}
 }
 
-bool Snake::KtRanAnQuaTo() 
+bool Snake::KtRanAnQuaTo()
 {
-	if      (toa_dox[0] == xqua_to - 1 && toa_doy[0] == yqua_to - 1) return true;
+	if (toa_dox[0] == xqua_to - 1 && toa_doy[0] == yqua_to - 1) return true;
 	else if (toa_dox[0] == xqua_to && toa_doy[0] == yqua_to - 1) return true;
 	else if (toa_dox[0] == xqua_to + 1 && toa_doy[0] == yqua_to - 1) return true;
 	else if (toa_dox[0] == xqua_to + 1 && toa_doy[0] == yqua_to) return true;
@@ -314,9 +313,9 @@ bool Snake::KtRanAnQuaTo()
 	return false;
 }
 
-bool Snake::KtRanDeQuaTo() 
+bool Snake::KtRanDeQuaTo()
 {
-	for (int i = 0; i < sl; i++) 
+	for (int i = 0; i < sl; i++)
 	{
 		if (toa_dox[i] == xqua_to && toa_doy[i] == yqua_to) return true;
 		else if (toa_dox[i] == xqua_to - 1 && toa_doy[i] == yqua_to - 1) return true;
@@ -332,17 +331,17 @@ bool Snake::KtRanDeQuaTo()
 }
 
 //xuất điểm
-void Snake::XuatDiem() 
+void Snake::XuatDiem()
 {
 	SetColor(3);
-	for (int i = 101; i <= 115; i++) 
+	for (int i = 101; i <= 115; i++)
 	{
 		gotoXY(i, 1);
 		std::cout << (char)207;
 		gotoXY(i, 5);
 		std::cout << (char)209;
 	}
-	for (int i = 1; i <= 5; i++) 
+	for (int i = 1; i <= 5; i++)
 	{
 		gotoXY(115, i);
 		std::cout << (char)198;
@@ -353,7 +352,7 @@ void Snake::XuatDiem()
 	std::cout << "Score: " << score;
 }
 
-void Snake::ChonTocDo() 
+void Snake::ChonTocDo()
 {
 
 	SetColor(11);
@@ -371,7 +370,7 @@ void Snake::ChonTocDo()
 		gotoXY(40, i);
 		std::cout << (char)176;
 	}
-	gotoXY(10, 0); 
+	gotoXY(10, 0);
 	std::cout << "CHOOSE SNAKE SPEED ";
 	gotoXY(15, 3); std::cout << "1. VERY SLOW";
 	gotoXY(15, 4); std::cout << "2. SLOW";
@@ -394,32 +393,32 @@ void Snake::ChonTocDo()
 		std::cout << (char)176;
 	}
 	int temp = _getch();
-	if (temp == (int)'1') 
+	if (temp == (int)'1')
 	{
 		sleep = 200;
 		tocdo = 1;
 	}
-	else if (temp == (int)'2') 
+	else if (temp == (int)'2')
 	{
 		sleep = 170;
 		tocdo = 2;
 	}
-	else if (temp == (int)'3') 
+	else if (temp == (int)'3')
 	{
 		tocdo = 3;
 		sleep = 140;
 	}
-	else if (temp == (int)'4') 
+	else if (temp == (int)'4')
 	{
 		sleep = 110;
 		tocdo = 4;
 	}
-	else if (temp == (int)'5') 
+	else if (temp == (int)'5')
 	{
 		sleep = 70;
 		tocdo = 5;
 	}
-	else if (temp == (int)'6') 
+	else if (temp == (int)'6')
 	{
 		sleep = 50;
 		tocdo = 6;
@@ -428,7 +427,7 @@ void Snake::ChonTocDo()
 	Setting();
 }
 
-void Snake::ChonMauRan() 
+void Snake::ChonMauRan()
 {
 
 	SetColor(11);
@@ -468,23 +467,23 @@ void Snake::ChonMauRan()
 		std::cout << (char)176;
 	}
 	int temp = _getch();
-	if (temp == (int)'1') 
+	if (temp == (int)'1')
 	{
 		mau_ran = 1;
 	}
-	else if (temp == (int)'2') 
+	else if (temp == (int)'2')
 	{
 		mau_ran = 2;
 	}
-	else if (temp == (int)'3') 
+	else if (temp == (int)'3')
 	{
 		mau_ran = 7;
 	}
-	else if (temp == (int)'4') 
+	else if (temp == (int)'4')
 	{
 		mau_ran = 4;
 	}
-	else if (temp == (int)'5') 
+	else if (temp == (int)'5')
 	{
 		mau_ran = 3;
 	}
@@ -495,12 +494,14 @@ void Snake::ChonMauRan()
 void Snake::Setting()
 {
 	SetColor(11);
-	gotoXY(10, 0); 
+	gotoXY(10, 0);
 	std::cout << "MAIN MENU";
-	gotoXY(21, 4); 
-	std::cout << "1. Play" << std::endl;
+	gotoXY(21, 4);
+	std::cout << "1. PLAY" << std::endl;
 	gotoXY(21, 5);
 	std::cout << "2. OPTION" << std::endl;
+	gotoXY(21, 6); 
+	std::cout << "3. EXIT" << std::endl; 
 	for (int i = 10; i <= 40; i++)
 	{
 		gotoXY(i, 1);
@@ -523,7 +524,7 @@ void Snake::Setting()
 	{
 
 		system("cls");
-		gotoXY(10, 0); std::cout << "OPTION"; 
+		gotoXY(10, 0); std::cout << "OPTION";
 		gotoXY(15, 5);
 		std::cout << "1. CHOOSE SNAKE SPEED" << std::endl;
 		gotoXY(15, 6);
@@ -545,21 +546,22 @@ void Snake::Setting()
 		int k = _getch();
 		if (k == (int)'1')
 		{
-			system("cls"); 
+			system("cls");
 			gotoXY(10, 1);
 			ChonTocDo();
 		}
-		else if (k== (int)'2')
+		else if (k == (int)'2')
 		{
 			system("cls");
 			gotoXY(10, 1);
 			ChonMauRan();
 		}
 	}
-} 
+	else if (temp == (int)'3') exit(0) ; 
+}
 
 //-------------------- hàm main -----------------------------------------------------
-int main() 
+int main()
 {
 	SetColor(8);
 	Snake s;
