7e0de6efd14dc082320f3a9d17f6a451af09d420
UIT.23520757 <168500784+khoaaminhh@users.noreply.github.com>
2024-05-13 15:02:13 +0700

    Thay đổi tên các hàm cho phù hợp file báo cáo

--- Snake game/mySnake.h
+++ Snake game/mySnake.h
@@ -3,31 +3,51 @@
 #include<iostream>	
 using namespace std;
 #define MAX 100
-class Snake {
-private:
-	int toa_dox[MAX] = { '0' };
-	int toa_doy[MAX] = { '0' };
-	int sl = 1;
-	int xqua = -1, yqua = -1;
-	int xqua_to = -1, yqua_to = -1;
-	int tocdo = 1;
-	int sleep = 200;
-	int mau_ran; 
 
-public:
-	void Play();
-	void VeTuong();
-	void KhoiTaoRan();
-	void VeRan();
-	void ChonTocDo(); 
-	void ChonMauRan();
-	void Setting();
-	bool kt_ran_an_qua(); 
-	void ve_qua(); 
-	void xu_li_ran_an_qua(); 
-	bool kiem_tra_ran_de_qua(); 
-	bool kiem_tra_ran_cham_than(); 
-	bool kiem_tra_ran_de_qua_to();
-	void xu_li_ran_an_qua_to(); 
-	bool kt_ran_an_qua_to(); 
+class Snake 
+{
+	private:
+		int toa_dox[MAX] = { '0' };
+		int toa_doy[MAX] = { '0' };
+		int sl = 1;
+		int tocdo = 1; 
+		int score = 0;
+		int mau_ran;
+		int sleep = 200;
+		static int kyluc;
+
+		int xqua = -1, yqua= -1; 
+		int xqua_to = -1, yqua_to = -1; 
+		int qua_to = 1; 
+
+	public:
+
+		void Setting(); 
+ 		void ChonTocDo();
+		void ChonMauRan();
+
+		void VeTuong();
+		void KhoiTaoRan();	
+		void VeRan();
+		void DiChuyen(int, int);
+		bool GameOver(int, int);
+		bool KtRanChamThan();
+
+		void XuatDiem();
+
+		void TaoQua();		
+		void VeQua();
+		bool KtRanDeQua();
+
+		bool KtRanAnQua(); 
+		void XuLiRanAnQua(); 
+
+		void TaoQuaTo(); 
+		void VeQuaTo(); 
+		bool KtRanAnQuaTo();
+		bool KtRanDeQuaTo();
+		void XuLiRanAnQuaTo(); 
+
+		void Play();
+
 };
